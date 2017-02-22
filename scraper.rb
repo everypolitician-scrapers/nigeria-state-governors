c1b3e3dd48d575dde11696a41464e0dec1c57840
Mark Longair <mhl@pobox.com>
2017-02-22 15:29:12 +0000

    Add some debugging output

--- scraper.rb
+++ scraper.rb
@@ -55,5 +55,6 @@ page = GovernorsList.new(response: Scraped::Request.new(url: list).response)
 
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
 page.governors.each do |governor|
+  puts "Saving #{governor}"
   ScraperWiki.save_sqlite([:name], governor.to_h)
 end
